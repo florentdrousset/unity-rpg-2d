e6d34003c6095405a4db4471ba3a9b153903b848
Florent Drousset <florent.drousset@gmail.com>
2020-06-16 23:20:25 +0200

    added Clamp method so camera doesnt go out of the map

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    // Every object in a Scene has a Transform. It's used to store and manipulate the position, rotation and scale of the object.
+    // Every Transform can have a parent, which allows you to apply position, rotation and scale hierarchically. 
     public Transform target;
     public float smoothing;
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +24,9 @@ public class CameraMovement : MonoBehaviour
        if (transform.position != target.position)
         {
             Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+
             transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
         } 
     }
